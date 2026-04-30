b57727fc7a15b53d498d388a647f5ceda346afda
bigbuddy9 <jessebarbato788@gmail.com>
2026-05-01 02:38:01 +0700

    Hero: drop logo above Cal embed (will be baked into Cal page itself)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import logoImg from "@/ScaleSolarLogo.png";
 import { site } from "@/lib/site";
 import VideoPlayer from "./VideoPlayer";
 import CalEmbed from "./CalEmbed";
@@ -19,13 +17,7 @@ export default function Hero() {
           <VideoPlayer embedUrl={videoEmbedUrl} thumbnail={videoThumbnail} title="Intro" />
         </div>
 
-        <div className="mx-auto mt-16 w-full max-w-3xl">
-          <Image
-            src={logoImg}
-            alt={site.brand}
-            priority
-            className="mx-auto mb-6 h-auto w-full max-w-[240px] md:max-w-[280px]"
-          />
+        <div className="mx-auto mt-12 w-full max-w-3xl">
           <CalEmbed calLink={site.calLink} namespace="hero" />
         </div>
       </div>
